6fc96ccbc0371cdbf5ee58300e96e4a538e63f8a
Wojtech99 <wojteknab@gmail.com>
2025-02-07 13:21:27 +0100

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        TestSeven<Integer, String> nameToLen = new TestSeven<>();
+
+        nameToLen.put(1, "a");
+        nameToLen.put(6, "wojtek");
+        nameToLen.put(5, "absxd");
+        nameToLen.put(3, "abc");
+        nameToLen.put(5, "ketjo");
+        System.out.println(nameToLen.get(5));
+        System.out.println(nameToLen.get(1));
+        System.out.println(nameToLen.get(6));
+        System.out.println(nameToLen.get(3));
+        System.out.println(nameToLen.get(7));
+    }}
